1f493cdf860c7abf4e8aa1e70011f223e7263a9d
Ondřej Kejkrt <o.kejkrt@gmail.com>
2026-08-26 18:16:42 +0200

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -16,23 +16,22 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	total_len;
 	
-	i = 0;
-	while (dst[i])
-		i++;
-	if (i < size)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len >= size)
+		return (size + src_len);
+	total_len = dst_len + src_len;
+	while (*src && dst_len < (size - 1))
 	{
-		while (*src && i < (size - 1))
-		{
-			dst[i] = *src++;
-			i++;
-		}
-		dst[i] = '\0';
+		dst[dst_len] = *src++;
+		dst_len++;
 	}
-	while (*src++)
-		i++;
-	return (i);
+	dst[dst_len] = '\0';
+	return (total_len);
 }
 
 int main(void)
